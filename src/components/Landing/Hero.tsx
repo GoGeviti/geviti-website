32c8a354ac8a5dbbabaf928f468a9c8ccf83efdf
Thomas Burgess <thomasburgess2000@gmail.com>
2023-10-08 22:26:59 -0500

    updated mobile

--- src/components/Landing/Hero.tsx
+++ src/components/Landing/Hero.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 
 import landingData from '@/constant/data/landing';
@@ -11,27 +11,60 @@ import { CustomLink } from '..';
 
 const heroData = landingData.hero;
 const Hero = () => {
+
+	const [isMobile, setIsMobile] = useState(false);
+
+	useEffect(() => {
+		const handleResize = () => {
+			setIsMobile(window.innerWidth <= 768);
+		};
+
+		handleResize(); // Initialize state
+		window.addEventListener('resize', handleResize);
+
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
+
 	return (
 		<div className='lg:px-3 lg:py-15px overflow-hidden'>
 			<Navbar
-				iconsMenu={ landingData.navbar.iconsMenu }
-				actionsMenu={ landingData.navbar.actionsMenu }
+				iconsMenu={landingData.navbar.iconsMenu}
+				actionsMenu={landingData.navbar.actionsMenu}
 			/>
 			<div className='bg-primary max-h-[850px] h-[calc(100vh-25px)] w-full lg:rounded-[19px] relative pt-11px lg:pt-5'>
 				<div className='absolute inset-0 w-full h-full'>
-					{ heroData.image && (
-						<div className='relative overflow-hidden w-full h-full lg:rounded-[19px]'>
-							<Image
-								src={ heroData.image }
-								alt='hero'
-								priority
-								className='object-cover'
-								fill
-								objectPosition='top'
-								quality={ 75 }
-							/>
-						</div>
-					) }
+					{isMobile ? (
+						(
+							<div className='relative overflow-hidden w-full h-full lg:rounded-[19px]'>
+								<Image
+									src={heroData.imageMobile}
+									alt='hero'
+									priority
+									className='object-cover'
+									fill
+									objectPosition='top'
+									quality={75}
+								/>
+							</div>
+						)
+					) : (
+						(
+							<div className='relative overflow-hidden w-full h-full lg:rounded-[19px]'>
+								<Image
+									src={heroData.image}
+									alt='hero'
+									priority
+									className='object-cover'
+									fill
+									objectPosition='top'
+									quality={75}
+								/>
+							</div>
+						)
+					)}
+
 				</div>
 
 				<div className='sm:hidden absolute top-0 inset-x-0 w-full h-[270px] bg-gradient-to-b from-primary to-[#181a1c00]' />
@@ -43,37 +76,37 @@ const Hero = () => {
 						<div className='container-center pb-11 lg:pb-[113px] h-full w-full flex flex-col items-center justify-end'>
 							<div className='text-center'>
 								<h2 className='text-grey-secondary font-BRSonoma font-semibold text-[10px] sm:text-xs lg:text-sm leading-[150%] lg:leading-6 uppercase tracking-0.11em'>
-									{ heroData.preTitle }
+									{heroData.preTitle}
 								</h2>
-								{ heroData.title && (
+								{heroData.title && (
 									<h1 className='mt-11px lg:mt-7px font-Poppins text-[21px] sm:text-2xl md:text-[32px] lg:text-[40px] sm:leading-[98%] -tracking-0.04em text-grey-secondary'>
-										<span dangerouslySetInnerHTML={ { __html: heroData.title } } />
+										<span dangerouslySetInnerHTML={{ __html: heroData.title }} />
 									</h1>
-								) }
+								)}
 
 								<div
 									id='main-keys-landing'
 									className='flex justify-center mt-7 sm:mt-9 space-x-5'>
 									<CustomLink
-										href={ heroData.btnCta.href }
-										externalLink={ heroData.btnCta.externalLink }
+										href={heroData.btnCta.href}
+										externalLink={heroData.btnCta.externalLink}
 										className='btn-cta-landing btn-secondary group'
-										aria-label={ heroData.btnCta.text }
+										aria-label={heroData.btnCta.text}
 									>
 										<span className='text-btn-cta-landing'>
-											{ heroData.btnCta.text }
+											{heroData.btnCta.text}
 										</span>
 
 										<ChevronRight className='arrow-btn-cta-landing' />
 									</CustomLink>
 									<CustomLink
-										href={ heroData.btnCta2.href }
-										externalLink={ heroData.btnCta2.externalLink }
+										href={heroData.btnCta2.href}
+										externalLink={heroData.btnCta2.externalLink}
 										className='btn-cta-landing  group '
-										aria-label={ heroData.btnCta2.text }
+										aria-label={heroData.btnCta2.text}
 									>
 										<span className='text-btn-cta-landing text-grey-secondary'>
-											{ heroData.btnCta2.text }
+											{heroData.btnCta2.text}
 										</span>
 
 										<ChevronRight className='arrow-btn-cta-landing text-grey-secondary' />
@@ -82,26 +115,26 @@ const Hero = () => {
 							</div>
 
 							<div className='grid grid-cols-3 sm:max-w-xl w-full mt-[53px] sm:mt-20'>
-								{ heroData.mainKeys.map((feature, featureIdx) => (
+								{heroData.mainKeys.map((feature, featureIdx) => (
 									<WrapperAnimation
-										key={ feature.text }
+										key={feature.text}
 										data-aos='zoom-in-up'
-										data-aos-delay={ `${ featureIdx * 100 }` }
+										data-aos-delay={`${featureIdx * 100}`}
 										data-aos-anchor='#main-keys-landing'
 										className='flex flex-col text-center items-center gap-y-1 md:gap-y-5px'
 									>
-										{ feature.image && (
+										{feature.image && (
 											<div className='relative overflow-hidden w-[21px] h-[21px] sm:w-[26px] sm:h-[26px]'>
 												<Image
-													src={ feature.image }
-													alt={ feature.text }
+													src={feature.image}
+													alt={feature.text}
 													sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
 													fill
 													loading='lazy'
 												/>
 											</div>
-										) }
-										<p className='font-Poppins leading-[225%] -tracking-0.04em text-grey-secondary text-[15.861px] sm:text-lg lg:text-xl'>{ feature.text }</p>
+										)}
+										<p className='font-Poppins leading-[225%] -tracking-0.04em text-grey-secondary text-[15.861px] sm:text-lg lg:text-xl'>{feature.text}</p>
 										<span className='inline-flex items-center gap-5px'>
 											<svg
 												xmlns='http://www.w3.org/2000/svg'
@@ -127,10 +160,10 @@ const Hero = () => {
 												</defs>
 											</svg>
 
-											<p className='text-grey-primary font-medium font-BRSonoma text-[11px] sm:text-sm'>{ feature.subtext }</p>
+											<p className='text-grey-primary font-medium font-BRSonoma text-[11px] sm:text-sm'>{feature.subtext}</p>
 										</span>
 									</WrapperAnimation>
-								)) }
+								))}
 							</div>
 						</div>
 					</div>
