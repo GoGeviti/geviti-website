b67d8a07ce3c66a3c1c14678bc1ddd9e08616a21
Ridho Idris <ridhoidris@gmail.com>
2023-11-16 06:51:33 +0700

    integration contact & partner contact

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,26 +1,19 @@
-'use client';
-import { useState } from 'react';
 import { NextPage } from 'next';
 
 import { ContactUsComponent } from '@/components';
 import { contactUsData } from '@/constant/data';
+import { getAllContactSubjects } from '@/services/products';
 
-const ContactUsPage: NextPage = () => {
-	const [firstName, setFirstName] = useState<string>('');
-	const [email, setEmail] = useState<string>('');
-	const [message, setMessage] = useState<string>('');
+const ContactUsPage: NextPage = async() => {
+
+	const subjectData = await getAllContactSubjects();
   
 	return (
 		<div className='flex min-h-screen flex-col w-full bg-grey-background'>
 			<ContactUsComponent.Hero hero={ contactUsData.hero }/>
 			<ContactUsComponent.Form
-				initialFirstName={ firstName }
-				initialEmail={ email }
-				initialMessage= { message }
-				onUpdateFirstName={ setFirstName }
-				onUpdateEmail={ setEmail }
-				onUpdateMessage={ setMessage }
-				subject={ contactUsData.form.subject }/>
+				subject={ subjectData.docs }
+			/>
 		</div>
 	);
 };
