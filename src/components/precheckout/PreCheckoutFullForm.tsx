174c21170f9128be151a669d38ea4296764189c2
Ridho Idris <ridhoidris@gmail.com>
2023-11-20 23:54:06 +0700

    fix error build

--- src/components/precheckout/PreCheckoutFullForm.tsx
+++ src/components/precheckout/PreCheckoutFullForm.tsx
@@ -1,4 +1,4 @@
-import { RefObject, useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Player } from '@lottiefiles/react-lottie-player';
 import styled, { keyframes } from 'styled-components';
 
