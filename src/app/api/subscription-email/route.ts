7230da7e9e91736b1f936be95c78c869909e008c
Uzair Sajid <uzee@uzeeinc.net>
2023-11-27 21:54:08 +0500

    fix: fix quality issues to trigger build

--- src/app/api/subscription-email/route.ts
+++ src/app/api/subscription-email/route.ts
@@ -1,7 +1,7 @@
-import SubscriptionEmail from "./templates/SubscriptionEmail";
-import { NextResponse } from "next/server";
-import { Resend } from "resend";
-import * as React from "react";
+import { NextResponse } from 'next/server';
+import { Resend } from 'resend';
+
+import SubscriptionEmail from './templates/SubscriptionEmail';
 interface requestPayload {
   email: string;
   name: string;
@@ -17,55 +17,55 @@ export interface subscriptionKeyResponse {
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 export async function POST(request: Request) {
-  const requestPayload = (await request.json()) as requestPayload;
-  try {
-    const { subscriptionKey } = await getSubscriptionKey();
-    const { data, error } = await sendSubscriptionEmail(
-      requestPayload,
-      subscriptionKey
-    );
-    if (error) {
-      return NextResponse.json(error);
-    }
+	const requestPayload = (await request.json()) as requestPayload;
+	try {
+		const { subscriptionKey } = await getSubscriptionKey();
+		const { data, error } = await sendSubscriptionEmail(
+			requestPayload,
+			subscriptionKey
+		);
+		if (error) {
+			return NextResponse.json(error);
+		}
 
-    return NextResponse.json({
-      status: 200,
-      message: "Email sent successfully!",
-      emailId: data?.id,
-    });
-  } catch (error) {
-    return NextResponse.json(error);
-  }
+		return NextResponse.json({
+			status: 200,
+			message: 'Email sent successfully!',
+			emailId: data?.id,
+		});
+	} catch (error) {
+		return NextResponse.json(error);
+	}
 }
 
 async function sendSubscriptionEmail(
-  requestPayload: requestPayload,
-  subscriptionKey: string
+	requestPayload: requestPayload,
+	subscriptionKey: string
 ) {
-  const { name, email } = requestPayload;
-  const emailPayload = {
-    from: "onboarding@resend.dev",
-    to: email,
-    subject: "Welcome to Geviti",
-    react: SubscriptionEmail({ name, subscriptionKey }),
-  };
-  return resend.emails.send(emailPayload);
+	const { name, email } = requestPayload;
+	const emailPayload = {
+		from: 'onboarding@resend.dev',
+		to: email,
+		subject: 'Welcome to Geviti',
+		react: SubscriptionEmail({ name, subscriptionKey }),
+	};
+	return resend.emails.send(emailPayload);
 }
 
 async function getSubscriptionKey() {
-  const apiUrl = `${process.env.SUBSCRIPTION_API_URL}/subscription-keys`; // Replace with your actual API endpoint
+	const apiUrl = `${process.env.SUBSCRIPTION_API_URL}/subscription-keys`; // Replace with your actual API endpoint
 
-  const response = await fetch(apiUrl, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.SUBSCRIPTION_TOKEN}`,
-    },
-  });
+	const response = await fetch(apiUrl, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${process.env.SUBSCRIPTION_TOKEN}`,
+		},
+	});
 
-  if (!response.ok) {
-    throw new Error(`HTTP error! Status: ${response.status}`);
-  }
+	if (!response.ok) {
+		throw new Error(`HTTP error! Status: ${response.status}`);
+	}
 
-  return (await response.json()) as subscriptionKeyResponse;
+	return (await response.json()) as subscriptionKeyResponse;
 }
