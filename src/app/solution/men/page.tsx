5350951b353eef7805fc05f83f4f64e66fe8e35b
star-works <111379816+star-works@users.noreply.github.com>
2024-04-12 17:09:43 +0530

    investment issue

--- src/app/solution/men/page.tsx
+++ src/app/solution/men/page.tsx
@@ -1,8 +1,9 @@
 import { Footer } from "@/components";
-import { Mission, RunningLogo } from "@/components/Landing";
+import { RunningLogo } from "@/components/Landing";
 import FrequentlyAskedQues from "@/components/MemberShip/FrequentlyAskedQues";
 import BloodworkPanel from "@/components/Solutions/BloodworkPanel";
 import Hero from "@/components/Solutions/Hero";
+import Investment from "@/components/Solutions/Investment";
 import OptimizedYourSelf from "@/components/Solutions/OptimizedYourSelf";
 import WellnessPro from "@/components/Solutions/WellnessPro";
 import EasyOnlineCare from "@/components/Solutions/easy-online-care/EasyOnlineCare";
@@ -19,7 +20,10 @@ const Solutions = () => {
         <OptimizedYourSelf />
         <FrequentlyAskedQues />
         <div className="flex flex-col gap-y-3.5">
-          <Mission />
+          <Investment
+            bgimagemobile="/images/solution_media/investment-bg-mobile.webp"
+            bgimagedesktop="/images/solution_media/investment-bg.webp"
+          />
           <Footer landingPage />
         </div>
       </div>
