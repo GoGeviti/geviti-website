adbbb18dcc88e5e9320cc9905d92bfe1ad05f02e
Anshul sharma <anshhulkaushik@gmail.com>
2024-04-12 11:40:39 +0530

    added-faq-fotter-in-men-page

--- src/app/solution/page.tsx
+++ src/app/solution/page.tsx
@@ -6,6 +6,9 @@ import Faq from "../../components/solution-content/Faq";
 import Investment from "../../components/solution-content/Investment";
 import WellnessPro from "@/components/solution-content/WellnessPro";
 import EasyOnlineCare from "@/components/solution-content/EasyOnlineCare";
+import FrequentlyAskedQues from "@/components/MemberShip/FrequentlyAskedQues";
+import { Mission } from "@/components/Landing"; 
+import { Footer } from "@/components";
 const Solutions = () => {
   return (
     <>
@@ -17,6 +20,11 @@ const Solutions = () => {
         <OptimizedYourSelf />
         <Faq />
         <Investment />
+        <FrequentlyAskedQues/>
+        <div className="flex flex-col gap-y-3.5">
+        <Mission />
+        <Footer landingPage />
+      </div>
       </div>
     </>
   );
