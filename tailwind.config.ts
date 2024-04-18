2af53b020f97bb7dde5878f9d0d37d3a0156cc8d
star-works <111379816+star-works@users.noreply.github.com>
2024-04-18 18:26:09 +0530

    working on fixing issues

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -149,6 +149,7 @@ const config: Config = {
           "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
       },
       boxShadow: {
+        custom: "0px 4px 21.1px 0px rgba(153, 212, 255, 0.75)",
         card: "0px 15px 30px 0px rgba(16, 24, 40, 0.10)",
         feature:
           "0px 8px 16px rgba(0, 0, 0, 0.25), inset 0px 6px 18px rgba(255, 255, 255, 0.15)",
