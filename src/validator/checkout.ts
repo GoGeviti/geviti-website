9e50f81d972438b941e218bdf033c1b61582000e
Anas Munir <anas.munir.92@gmail.com>
2024-06-05 23:29:16 +0500

    changed some validations

--- src/validator/checkout.ts
+++ src/validator/checkout.ts
@@ -1,36 +1,38 @@
-import { differenceInYears } from 'date-fns';
+import { differenceInYears, isValid } from 'date-fns';
 import * as yup from 'yup';
 
-{ /* eslint-disable @typescript-eslint/no-explicit-any */ }
+{
+	/* eslint-disable @typescript-eslint/no-explicit-any */
+}
 
 export const FormCheckoutSchema = yup.object().shape({
-	first_name: yup.string()
+	first_name: yup.string().label('First name')
 		.required()
-		.label('First name'),
-	last_name: yup.string()
-		.label('Last name'),
+		.matches(/^[a-zA-ZÀ-ÖÙ-öù-ÿĀ-žḀ-ỿ\/]+$/, 'Please enter valid First name')
+		.max(40),
+	last_name: yup.string().label('Last name')
+		.matches(/^[a-zA-ZÀ-ÖÙ-öù-ÿĀ-žḀ-ỿ\/]+$/, 'Please enter valid Last name')
+		.max(40),
 	email: yup.string().email()
 		.required()
 		.label('Email'),
-	phone_number: yup.string()
-		.required()
+	phone_number: yup.string().required()
 		.label('Phone number'),
-	address_1: yup.string(),
+	address_1: yup.string().label('Address')
+		.required(),
 	address_2: yup.string(),
 	city: yup.string().required()
 		.label('City'),
 	zip_code: yup.string().required()
 		.label('Zip code'),
-	state: yup.string()
-		.required()
+	state: yup.string().required()
 		.label('State'),
-	gender: yup.string()
-		.required()
+	gender: yup.string().required()
 		.label('Sex'),
-	birthdate: yup.date()
+	birthdate: yup
+		.date()
 		.required()
 		.label('Date of birth')
-		.test('birthdate', 'Must be 18 years or older', (value: any) => {
-			return differenceInYears(new Date(), new Date(value)) >= 18; ;
-		}),
+		.test('birthdate', 'Enter a valid date', value => isValid(value))
+		.test('birthdate', 'Must be 18 years or older', value => differenceInYears(new Date(), new Date(value)) >= 18),
 });
