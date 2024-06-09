7ac41d2dfd19b518e15bc62ed19de22e1a8e5e02
Anas Munir <anas.munir.92@gmail.com>
2024-06-09 15:21:31 +0500

    separated out total cal component

--- src/components/Checkout/StripeCheckout/StripeCheckout.tsx
+++ src/components/Checkout/StripeCheckout/StripeCheckout.tsx
@@ -1,8 +1,6 @@
 'use client';
 
-import React, {
-	FC, Fragment, useCallback, useEffect, useMemo, useState
-} from 'react';
+import React, { FC, useCallback, useEffect, useState } from 'react';
 import { AiFillCloseCircle } from 'react-icons/ai';
 import { useRouter } from 'next/navigation';
 import { toast } from 'sonner';
@@ -20,7 +18,6 @@ import {
 	DiscountReturnType,
 	InitialOfferingsReturnType,
 	MembershipOfferingsReturnType,
-	PROMO_TYPE,
 	TempUser,
 } from '../api/types';
 
@@ -28,6 +25,7 @@ import CheckoutItem from './CheckoutItem';
 import DiscountForm from './DiscountForm';
 import PageHeader from './PageHeader';
 import StripeElementsProvider from './StripeElementsProvider';
+import { TotalCalc } from './TotalCalculation';
 
 type PageProps = {
 	searchParams: { [key: string]: string | string[] | undefined; };
@@ -207,63 +205,4 @@ const StripeCheckout: FC<PageProps> = ({ searchParams }) => {
 	);
 };
 
-interface ITotalCalc {
-  productPrice: number;
-  membershipPrice: number;
-  discount: DiscountReturnType | null;
-  setTotalPrice: React.Dispatch<React.SetStateAction<number | undefined>>;
-}
-const TotalCalc: FC<ITotalCalc> = ({
-	productPrice,
-	membershipPrice,
-	discount,
-	setTotalPrice,
-}) => {
-
-	const total = useMemo(
-		() => (productPrice + membershipPrice).toFixed(2),
-		[productPrice, membershipPrice]
-	);
-
-	const totalDue = useMemo(() => {
-		if (discount?.coupon_exist) {
-			const due = (membershipPrice + discount.coupon_details.discounted_price).toFixed(2);
-			setTotalPrice(Number(due));
-			return due;
-		}
-		setTotalPrice(Number(total));
-		return total;
-	}, [total, discount]);
-
-	return (
-		<div className='flex justify-between py-12'>
-			<div className='flex flex-col'>
-				<p className='text-grey-primary text-sm'>Total</p>
-				{ discount?.coupon_exist && (
-					<p className='text-grey-primary py-6 text-sm'>
-            Coupon - { discount.coupon_details.keyword }
-					</p>
-				) }
-				<p className='text-white text-lg py-6'>Total due</p>
-			</div>
-			<div className='flex flex-col text-right'>
-				<p className='text-grey-primary text-sm'>${ total }</p>
-				{ discount?.coupon_exist && (
-					<p className='text-grey-primary py-6 text-sm'>
-						{ discount?.coupon_details.promo_type === PROMO_TYPE.AMOUNT_OFF ? (
-							<Fragment>
-								-${ discount.coupon_details.amount_off }
-							</Fragment>
-						) : (
-							<Fragment>
-								-{ discount.coupon_details.amount_off }%
-							</Fragment>
-						) }
-					</p>
-				) }
-				<p className='text-white text-lg py-6'>${ totalDue }</p>
-			</div>
-		</div>
-	);
-};
 export default StripeCheckout;
