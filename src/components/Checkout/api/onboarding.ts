c991dd6b1b42fe478becdbd47125215bc947044c
Anas Munir <anas.munir.92@gmail.com>
2024-06-10 16:41:57 +0500

    sending user token as a header in Apis

--- src/components/Checkout/api/onboarding.ts
+++ src/components/Checkout/api/onboarding.ts
@@ -8,6 +8,7 @@ import {
 	ErrorResponse,
 	InitialOfferingsReturnType,
 	MembershipOfferingsReturnType,
+	TempUser,
 	TempUserDataParams,
 	TempUserReturnType,
 	WaitListParams,
@@ -16,7 +17,7 @@ import {
 const onboardingApiUrl = process.env.NEXT_PUBLIC_ONBOARDING_API_URL || '';
 const token = process.env.NEXT_PUBLIC_ONBOARDING_TOKEN;
 
-const headers = { Authorization: `Bearer ${token}`, 'Content-Type': 'application/json' };
+const headers = { 'Content-Type': 'application/json' };
 
 const processResponse = async <T>(res: Response): Promise<T> => {
 	if (res.ok) {
@@ -31,6 +32,15 @@ const processError = (error: unknown) => {
 	return Promise.reject(err?.message?.toString() || 'Something went wrong');
 }
 
+const fetchUserToken = () => {
+	const user = sessionStorage.getItem('temp_user');
+	if (!user) {
+		return null;
+	}
+	const tempUser = JSON.parse(user) as TempUser;
+	return tempUser?.token ? tempUser.token : null;
+}
+
 export const addTempUser = async(params: TempUserDataParams): Promise<TempUserReturnType> => {
 	try {
 		const res = await fetch(
@@ -38,7 +48,10 @@ export const addTempUser = async(params: TempUserDataParams): Promise<TempUserRe
     `,
 			{
 				method: 'POST',
-				headers,
+				headers: {
+					Authorization: `Bearer ${token}`,
+					...headers,
+				},
 				cache: 'no-store',
 				body: JSON.stringify(params),
 			}
@@ -51,9 +64,15 @@ export const addTempUser = async(params: TempUserDataParams): Promise<TempUserRe
 
 export const joinWaitList = async(params: WaitListParams) => {
 	try {
+		const userToken = fetchUserToken();
+		if (!userToken) throw { message: 'No user found' };
+
 		const res = await fetch(`${onboardingApiUrl}/v2/users/waitlist`, {
 			method: 'POST',
-			headers,
+			headers: {
+				Authorization: `Token ${userToken}`,
+				...headers,
+			},
 			cache: 'no-store',
 			body: JSON.stringify(params),
 		});
@@ -65,9 +84,14 @@ export const joinWaitList = async(params: WaitListParams) => {
 
 export const getInitialOfferings = async(): Promise<InitialOfferingsReturnType[]> => {
 	try {
+		const userToken = fetchUserToken();
+		if (!userToken) throw { message: 'No user found' };
 		const res = await fetch(`${onboardingApiUrl}/billing/offerings-info?billingType=initial-package`, {
 			method: 'GET',
-			headers,
+			headers: {
+				Authorization: `Token ${userToken}`,
+				...headers,
+			},
 		});
 		const data = await processResponse<InitialOfferingsReturnType[]>(res);
 		return data.map(it => ({ ...it, price: Number(it.price), first_time_payment: Number(it.first_time_payment) }))
@@ -95,12 +119,18 @@ export const getMembershipOfferings = async(): Promise<MembershipOfferingsReturn
 
 export const getDiscount = async(params: DiscountParams): Promise<DiscountReturnType> => {
 	try {
+		const userToken = fetchUserToken();
+		if (!userToken) throw { message: 'No user found' };
+
 		const res = await fetch(
 			`${onboardingApiUrl}/billing/offerings-coupon?keyword=${params.keyword}&offering_id=${params.offering_id}&price=${params.price}
     `,
 			{
 				method: 'GET',
-				headers,
+				headers: {
+					Authorization: `Token ${userToken}`,
+					...headers,
+				},
 				cache: 'no-store',
 			}
 		);
@@ -121,12 +151,17 @@ export const getDiscount = async(params: DiscountParams): Promise<DiscountReturn
 
 export const checkout = async(params: CheckoutParams): Promise<CheckoutResponseType> => {
 	try {
+		const userToken = fetchUserToken();
+		if (!userToken) throw { message: 'No user found' };
 		const res = await fetch(
 			`${onboardingApiUrl}/billing/checkout
     `,
 			{
 				method: 'POST',
-				headers,
+				headers: {
+					Authorization: `Token ${userToken}`,
+					...headers,
+				},
 				cache: 'no-store',
 				body: JSON.stringify(params),
 			}
