62f66f5cbc94ab6da3619498da3933c3b143fa20
Ridho Idris <ridhoidris@gmail.com>
2024-08-29 05:43:07 +0700

    stripe font poppins

--- src/components/Checkout/StripeCheckout/StripeForm.tsx
+++ src/components/Checkout/StripeCheckout/StripeForm.tsx
@@ -359,6 +359,14 @@ const StripeForm: FC<StripeFormProps> = ({
 									stripe={ stripePromise }
 									options={ {
 										clientSecret: sessionSecretS,
+										appearance: {
+											variables: {
+												fontFamily: 'Poppins',
+											},
+										},
+										fonts: [{
+											cssSrc: 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap',
+										}],
 									} }
 								>
 									<StripePaymentElement
