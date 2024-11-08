db259b85978b45623b5df08d678bb6a912d09be9
Ridho Idris <ridhoidris@gmail.com>
2024-11-08 13:48:45 +0700

    rollback redirection

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,28 +1,14 @@
 import ReactGA from 'react-ga4';
-import type { NextRequest } from 'next/server';
-import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
 
 const trackingId = 'G-9NMVVP83JB';
 
+// This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
 
-	 const hostname = request.headers.get('host') || ''
 	const { pathname } = new URL(request.url);
-
-	// Debug logging (check Railway logs)
-	console.log('Middleware running for:', {
-		hostname,
-		pathname,
-		url: request.url
-	})
-
-	// Redirect gogeviti.com to www.gogeviti.com, but avoid redirecting if already on www.gogeviti.com
-	if (hostname === 'gogeviti.com') {
-		const url = new URL(request.url);
-		url.hostname = 'www.gogeviti.com';
-		return NextResponse.redirect(url);
-	}
-
+    
 	// Redirect /pickleballkingdom to /
 	if (pathname === '/pickleballkingdom') {
 		ReactGA.initialize(trackingId);
@@ -30,31 +16,23 @@ export function middleware(request: NextRequest) {
 		return NextResponse.redirect(new URL('/', request.url));
 	}
 
-	// Redirect to App Store or Play Store if the path is /mobile
-	if (pathname === '/mobile') {
-		const userAgent = request.headers.get('user-agent')?.toLowerCase();
-		if (userAgent) {
-			if (userAgent.includes('iphone')) {
-				return NextResponse.redirect(process.env.NEXT_PUBLIC_APPLE_STORE_URL);
-			} else if (userAgent.includes('android')) {
-				return NextResponse.redirect(process.env.NEXT_PUBLIC_PLAY_STORE_URL);
-			} else {
-				return NextResponse.redirect(new URL('/', request.url));
-			}
+	const userAgent = request.headers.get('user-agent')?.toLowerCase();
+
+	if (userAgent) {
+		if (userAgent.includes('iphone')) {
+			return NextResponse.redirect(process.env.NEXT_PUBLIC_APPLE_STORE_URL);
+		} else if (userAgent.includes('android')) {
+			return NextResponse.redirect(process.env.NEXT_PUBLIC_PLAY_STORE_URL);
+		} else {
+			return NextResponse.redirect(new URL('/', request.url))
 		}
 	}
 
-	// Continue with the request for all other cases
+	// Continue with the request if no user agent match
 	return NextResponse.next();
 }
-
-// Match paths for middleware to apply to specific paths
+ 
+// See "Matching Paths" below to learn more
 export const config = {
-	matcher: [
-		// Add the catch-all route for www redirect
-		'/((?!api|_next|_static|images|videos|meta|lottie|files|_vercel|[\\w-]+\\.\\w+).*)',
-		// Keep your existing routes
-		'/mobile',
-		'/pickleballkingdom'
-	]
-};
+	matcher: ['/mobile', '/pickleballkingdom'],
+}
