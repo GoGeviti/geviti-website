ad3524812cbcc9953ad23f2809e04f7838ba2452
Ridho Idris <ridhoidris@gmail.com>
2025-04-01 15:09:48 +0700

    feat: Add email opt-in form to blog page

--- src/app/blog/[id]/page.client.tsx
+++ src/app/blog/[id]/page.client.tsx
@@ -1,6 +1,9 @@
 // import { NextPage } from 'next';
 'use client'
-import { useRef } from 'react';
+import { FormEvent, useRef, useState } from 'react';
+import { AiFillCloseCircle } from 'react-icons/ai';
+import Image from 'next/image';
+import { toast } from 'sonner';
 
 import { ArticleComponent, BlogComponent } from '@/components';
 import { Blocks } from '@/components/Blocks';
@@ -9,12 +12,15 @@ import { articleData } from '@/constant/data';
 import { calculateReadingTime } from '@/helpers/calculateReadingTime';
 import clsxm from '@/helpers/clsxm';
 import { Post } from '@/payload/payload-types';
+import { createDiscount } from '@/services/checkout';
 
 export const PageClient: React.FC<{
   post: Post
 }> = ({ post }) => {
 
 	const headingRefs = useRef<{[key: string]: HTMLElement | null}>({});
+	const emailRef = useRef<HTMLInputElement>(null);
+	const [loading, setLoading] = useState(false)
 
 	const scrollToHeading = (id: string) => {
 		const element = headingRefs.current[id];
@@ -22,7 +28,6 @@ export const PageClient: React.FC<{
 			const offset = 100; // Adjust this value based on your header height
 			const elementPosition = element.getBoundingClientRect().top;
 			const offsetPosition = elementPosition + window.scrollY - offset;
-			
 			window.scrollTo({
 				top: offsetPosition,
 				behavior: 'smooth'
@@ -31,6 +36,44 @@ export const PageClient: React.FC<{
 	};
 
 	const readingTime = calculateReadingTime(post.layout);
+
+	const handleOptin = async(e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const email = emailRef.current?.value;
+		// Validasi email
+		if (!email || !email.includes('@')) {
+			toast.error('Please enter a valid email address');
+			return;
+		}
+		setLoading(true)
+		try {
+			const result = await createDiscount({
+				email: email ?? '',
+				options: 'RnTM7m',
+				name: '',
+				state: '',
+				phone_number: ''
+			});
+			if (result.status === 'OK' && result.profileId) {
+				setLoading(false);
+				if (emailRef.current) {
+					emailRef.current.value = '';
+				}
+				toast.success(
+					'Thank you! Look out for an email from us soon.'
+				);
+				return;
+			} else {
+				setLoading(false);
+				throw new Error(result.message || 'Failed to subscribe. Please try again later.');
+			}
+		} catch (error:any) {
+			setLoading(false);
+			toast.error(error?.message as string, {
+				icon: <AiFillCloseCircle className='h-5 w-5 text-danger' />,
+			});
+		}
+	};
 
 	return (
 		<div className='flex min-h-screen flex-col w-full bg-grey-background'>
@@ -92,6 +135,35 @@ export const PageClient: React.FC<{
 								return null;
 							}) }
 						</ul>
+						<form
+							onSubmit={ handleOptin }
+							className='relative rounded-xl overflow-hidden'>
+							<Image
+								src='/images/blog/optin.jpg'
+								alt='optin'
+								width={ 325 }
+								height={ 244 }
+								className='w-full h-full absolute inset-0 object-cover'
+							/>
+							<div className='py-[21px] relative z-10 overflow-hidden rounded-xl text-white px-3.5 text-center flex flex-col gap-6 justify-center items-center'>
+								<div>
+									<h4 className='h4 text-white'>Get access</h4>
+									<p className='body-extra-small text-white'>Get access to our 5 Blood Biomarkers<br className='max-lg:hidden'/> of Longevity PDF:</p>
+								</div>
+								<input
+									ref={ emailRef }
+									type='email'
+									required
+									placeholder='enter your email'
+									className='w-full rounded-md h-[36px] backdrop-blur-[20px] font-Poppins text-xs bg-white/10 border border-grey-200/50' />
+								<button
+									type='submit'
+									disabled={ loading }
+									className='bg-white h-[36px] hover:bg-gray-100 transition-colors w-full rounded-full font-Poppins text-xs text-primary '>
+									{ loading ? 'Loading...' : 'Get access' }
+								</button>
+							</div>
+						</form>
 					</div>
 					<div className='max-w-screen-md w-full'>
 						<Blocks
