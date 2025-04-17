703c91365c224c79f73a5ea336b07118d568af09
Ridho Idris <ridhoidris@gmail.com>
2025-04-18 00:42:30 +0700

    feat: Enhance product category page with price comparison section

--- src/app/solution/men/[category]/[product_id]/page.tsx
+++ src/app/solution/men/[category]/[product_id]/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Image from 'next/image';
 import { notFound } from 'next/navigation'
 
-import {
-	Footer, FrequentlyAskedQuestions, Line, MarketingComponent, RunningLogo
-} from '@/components'
+import gevitiLogo from '@/assets/precheckout/geviti-logo.svg';
+import { Footer, FrequentlyAskedQuestions, Line, RunningLogo } from '@/components'
+import LongevitiBlendComponent from '@/components/LongevitiBlend';
 import Navbar from '@/components/Navbar/Landing'
 import { ViewOtherCategories } from '@/components/Solutions'
 import { solutionData } from '@/constant/data'
@@ -71,21 +72,58 @@ const ProductCategorySingle = async(props:{
 				<Description data={ productDataSingle.stats }/>
 			</div> */ }
 			<Line className='max-lg:hidden'/>
-			<License/>
-			<Tested data={ productDataSingle.testing }/>
+			<Why data={ productDataSingle.why }/>
+			<FrequentlyAskedQuestions data={ solutionData.faq.data } />
+			{ /* Compare price */ }
+			<div className='lg:px-3 my-16 lg:my-[124px]'>
+				<div className='container-center'>
+					<div>
+						<h3 className='text-2xl lg:text-4xl font-medium text-primary whitespace-nowrap'>
+							More, for less with Geviti
+						</h3>
+						<p className='body-small max-w-[600px]'>Lorem ipsum dolor sit amet consectetur. Sed posuere aliquet malesuada gravida velit massa nunc. Nunc at nunc nibh pretium</p>
+					</div>
+					<div className='mt-10 flex lg:flex-row flex-col items-center gap-5 justify-between' >
+						<div>
+							<h6 className='h6'>Compare price</h6>
+							<h3 className='h3 leading-none'>{ productDataSingle.name }</h3>
+						</div>
+						<div
+							className='bg-white max-lg:w-full flex flex-col items-center justify-center p-9 text-center rounded-[20px] shadow-price-comparison'>
+							<Image
+								src={ gevitiLogo }
+								width={ 85 }
+								height={ 20 }
+								alt=''
+								unoptimized />
+							<h6 className='h6 text-green-alert mt-4'>$99.99/mo</h6>
+						</div>
+						<div className='flex flex-col items-center max-lg:shadow-price-comparison max-lg:w-full p-9 rounded-[20px] justify-center text-center'>
+							<h5 className='h5 text-grey-primary'>Blokes</h5>
+							<h6 className='h6 text-yellow-alert mt-2.5'>$129.00/mo</h6>
+						</div>
+						<div className='flex flex-col items-center max-lg:shadow-price-comparison max-lg:w-full p-9 rounded-[20px] justify-center text-center'>
+							<h5 className='h5 text-grey-primary'>LifeForce</h5>
+							<h6 className='h6 text-yellow-alert mt-2.5'>$129.00/mo</h6>
+						</div>
+					</div>
+				</div>
+			</div>
+			<LongevitiBlendComponent.HowItWorks />
 			<Biomakers
 				stats={ productDataSingle.stats }
 				items={ productDataSingle.treatmentOptions?.map(e => e.name) ?? [] } />
-			<Why data={ productDataSingle.why }/>
+			<div className='mt-[42px]'>
+				<RunningLogo />
+			</div>
+			<License/>
+			<Tested data={ productDataSingle.testing }/>
 			<ViewOtherCategories
 				baseUrl={ `/men/${category}` }
 				isProduct
 				data={ productsData } />
-			<div className='mt-[42px] lg:mt-[87px]'>
-				<RunningLogo />
-			</div>
-			<MarketingComponent.Testimonials />
-			<FrequentlyAskedQuestions data={ solutionData.faq.data } />
+
+			{ /* <MarketingComponent.Testimonials /> */ }
 			<Footer landingPage />
 		</div>
 	)
